28799c4895fe125a55dcf66cd5923c7644faf8bb
delmont <pazxx219@gmail.com>
2018-05-02 21:32:55 +0900

    ファイル名変更

--- Multiplicatron 4000/client/adder.js
+++
@@ -1,4 +0,0 @@
-const add = (num1, num2) => {
-    return num1 +  num2;
-};
-module.exports = add;
--- multiplicatron4000/client/app.js
+++ multiplicatron4000/client/app.js
@@ -1,26 +1,33 @@
-const add = require('./adder.js');
-console.log(add(5,6));
+const add = require('./multiply.js');
 
 class ViewManager{
     connectEventHandlers(){
+        //テストコード
+        let singleSelectNum = document.getElementsByClassName('input-num');
+
         document.getElementById('form-numbers')
             .addEventListener('submit',this.onSubmit.bind(this));
     }
 
     onSubmit(event){
         event.preventDefault();
+        /*
+        
         let num1 = document.getElementById('input-num1').value;
         let num2 = document.getElementById('input-num2').value;
         num1 = parseInt(num1,10);
         num2 = parseInt(num2,10);
         
-        const sum = add(num1, num2);
+        const sum = multiply(num1, num2);
+        */
+       let singleSelectNum = document.getElementsByClassName('input-num');
+
 
         this.renderSum(sum);
     }
 
     renderSum(sum){
-        document.querySelector('.sum').textContent = sum;
+        document.querySelector('.ans').textContent = sum;
     }
 
 }
