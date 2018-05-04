1200ba0274fd956847d27c863a860fa36d10cd52
delmont <pazxx219@gmail.com>
2018-05-04 10:11:19 +0900

    eslint追加＆for見直し

--- multiplicatron4000/client/app.js
+++ multiplicatron4000/client/app.js
@@ -1,4 +1,4 @@
-const add = require('./multiply.js');
+//const add = require('./multiply.js');
 
 class ViewManager{
     connectEventHandlers(){
@@ -6,27 +6,20 @@ class ViewManager{
         console.log(document.getElementById('cal_button'));
         /* end test */
 
-        document.getElementById('cal_button')
-            .addEventListener('submit',this.onCal.bind(this));
-        document.getElementById('add_button')
-            .addEventListener('submit',this.onAdd.bind(this));
+        document.getElementById('form-numbers')
+            .addEventListener('submit', this.onSubmit.bind(this), false);
     }
 
-    onCal(event){
+    onSubmit(event){
+        /* test code */
+        console.log(event.target.id);
+        /* test end */
+
         event.preventDefault();
-        /*
-        
-        let num1 = document.getElementById('input-num1').value;
-        let num2 = document.getElementById('input-num2').value;
-        num1 = parseInt(num1,10);
-        num2 = parseInt(num2,10);
-        
-        const sum = multiply(num1, num2);
-        */
         let allNumObj = document.querySelectorAll('.num-tag');
         let forAnsVar = 1;
-        for(let tmpNum in allNumObj){
-            forAnsVar *= parseInt(tmpNum.value,10);
+        for(let tmpNum of allNumObj){
+            forAnsVar *= parseInt(tmpNum.value, 10);
         }
 
         this.renderSum(forAnsVar);
@@ -35,12 +28,6 @@ class ViewManager{
     renderSum(sum){
         document.getElementById('ans').textContent = sum;
     }
-
-    onAdd(){
-        event.preventDefault();    
-
-    }
-
 }
 
 const mg = new ViewManager();
--- multiplicatron4000/client/index.js
+++ multiplicatron4000/client/index.js
@@ -7,26 +7,19 @@ class ViewManager{
         console.log(document.getElementById('cal_button'));
         /* end test */
 
-        document.getElementById('cal_button')
-            .addEventListener('submit',this.onCal.bind(this));
-        document.getElementById('add_button')
-            .addEventListener('submit',this.onAdd.bind(this));
+        document.getElementById('form-numbers')
+            .addEventListener('submit', this.onSubmit.bind(this), false);
     }
 
-    onCal(event){
+    onSubmit(event){
+        /* test code */
+        console.log(event.target.id);
+        /* test end */
+
         event.preventDefault();
-        /*
-        
-        let num1 = document.getElementById('input-num1').value;
-        let num2 = document.getElementById('input-num2').value;
-        num1 = parseInt(num1,10);
-        num2 = parseInt(num2,10);
-        
-        const sum = multiply(num1, num2);
-        */
         let allNumObj = document.querySelectorAll('.num-tag');
         let forAnsVar = 1;
-        for(let tmpNum in allNumObj){
+        for(let tmpNum of allNumObj){
             forAnsVar *= parseInt(tmpNum.value,10);
         }
 
@@ -36,12 +29,6 @@ class ViewManager{
     renderSum(sum){
         document.getElementById('ans').textContent = sum;
     }
-
-    onAdd(){
-        event.preventDefault();    
-
-    }
-
 }
 
 const mg = new ViewManager();
